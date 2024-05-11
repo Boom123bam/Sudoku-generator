53de1e1326456efa16b62e48d08c75aaebad3a45
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-05-11 08:50:28 +0100

    Templ grid

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
 	"sudoku-generator/sudoku"
+
+	"github.com/a-h/templ"
 )
 
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/"):]
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// }
+
 func main() {
-	for i := 0; i < 100; i++ {
-		fmt.Println(i)
-		grid := sudoku.NewGrid(true)
-		fmt.Println(grid)
-		fmt.Println(grid.Solve(0))
-	}
+	grid := sudoku.NewGrid(true)
+	component := sudokuGrid(grid)
+	http.Handle("/", templ.Handler(component))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
