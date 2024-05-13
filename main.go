89fa9e6f297889ba4ac8ba05bf808fa7bf15e4c3
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-05-13 08:48:14 +0100

    Form

--- main.go
+++ main.go
@@ -1,16 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type FormData struct {
+	Pages          int
+	SudokusPerPage int
+}
+
+type FormConstraints struct {
+	MaxPages              int
+	SudokusPerPageOptions []int
+}
+
+var formConstraints = FormConstraints{
+	MaxPages:              10,
+	SudokusPerPageOptions: []int{1, 2, 6},
+}
+
+func (formData FormData) IsValid() bool {
+	if formData.Pages < 0 || formData.Pages > formConstraints.MaxPages {
+		return false
+	}
+	for _, n := range formConstraints.SudokusPerPageOptions {
+		if n == formData.SudokusPerPage {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	templates := template.Must(template.ParseGlob("views/*.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "index", struct{}{})
+		templates.ExecuteTemplate(w, "index", formConstraints)
+	})
+
+	http.HandleFunc("/generate", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		formData := FormData{}
+		err := error(nil)
+		formData.Pages, err = strconv.Atoi(r.Form.Get("pages"))
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		formData.SudokusPerPage, err = strconv.Atoi(r.Form.Get("sudokusPerPage"))
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		if !formData.IsValid() {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		fmt.Println(formData)
+
+		templates.ExecuteTemplate(w, "sudokus", struct{}{})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
